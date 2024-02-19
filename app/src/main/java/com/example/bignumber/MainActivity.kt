20367c74422203ee275fdbca1a4d2409855b789c
EricWRogers <ericwilliamrogers@gmail.com>
2024-02-19 09:26:33 -0600

    remove unused import

--- app/src/main/java/com/example/bignumber/MainActivity.kt
+++ app/src/main/java/com/example/bignumber/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
