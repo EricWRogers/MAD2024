0b136446a39d8038062c144a59d26926e68f2325
EricWRogers <ericwilliamrogers@gmail.com>
2024-04-05 09:23:02 -0500

    Fragment push

---
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment(R.layout.fragment1) {}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+
+        var button1 = findViewById<Button>(R.id.fragment_button_one);
+        button1.setOnClickListener {
+            var myFragment = Fragment1()
+            replaceFragment(myFragment)
+        }
+
+        var button2 = findViewById<Button>(R.id.fragment_button_two);
+        button2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+}
