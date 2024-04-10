a81b02e88b51deedaf965d7fbe9d881b26a498e1
EricWRogers <ericwilliamrogers@gmail.com>
2024-04-10 09:28:15 -0500

    Fragment passing data in from activity

--- app/src/main/java/com/example/myapplication/Fragment1.kt
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -1,7 +1,24 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class Fragment1 : Fragment(R.layout.fragment1) {}
+class Fragment1 : Fragment(R.layout.fragment1) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+
+        val rootView = inflater.inflate(R.layout.fragment1, container, false)
+        val title = requireArguments().getString("title") ?: "title not found"
+
+        rootView.findViewById<TextView>(R.id.title).text = title
+        return rootView
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,26 +11,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
 
-        var button1 = findViewById<Button>(R.id.fragment_button_one);
+        val button1 = findViewById<Button>(R.id.fragment_button_one);
         button1.setOnClickListener {
-            var myFragment = Fragment1()
-            replaceFragment(myFragment)
+            val bundle = Bundle()
+            bundle.putString("title", "Fragment 1 From Activity")
+
+            val myFragment = Fragment1()
+            replaceFragment(myFragment, bundle)
         }
 
-        var button2 = findViewById<Button>(R.id.fragment_button_two);
+        val button2 = findViewById<Button>(R.id.fragment_button_two);
         button2.setOnClickListener {
-            replaceFragment(Fragment2())
+            val bundle = Bundle()
+            bundle.putString("title", "Fragment 2 From Activity")
+
+            replaceFragment(Fragment2(), bundle)
         }
     }
 
-    private fun replaceFragment(fragment: Fragment)
+    private fun replaceFragment(fragment: Fragment, bundle: Bundle)
     {
+        fragment.arguments = bundle
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragment_container, fragment)
