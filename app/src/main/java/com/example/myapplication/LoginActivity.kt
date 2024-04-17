869c6a69175e9904f0e90d34dffbad91183fcde5
EricWRogers <ericwilliamrogers@gmail.com>
2024-04-17 08:59:18 -0500

    test

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        auth = Firebase.auth
+
+        val buttonLogin = findViewById<Button>(R.id.login_button)
+        buttonLogin.setOnClickListener {
+            val editTextEmail = findViewById<EditText>(R.id.email)
+            val editTextPassword = findViewById<EditText>(R.id.password)
+
+            val email = editTextEmail.toString()
+            val password = editTextPassword.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication Success.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,41 +1,14 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val button1 = findViewById<Button>(R.id.fragment_button_one);
-        button1.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString("title", "Fragment 1 From Activity")
-
-            val myFragment = Fragment1()
-            replaceFragment(myFragment, bundle)
-        }
-
-        val button2 = findViewById<Button>(R.id.fragment_button_two);
-        button2.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString("title", "Fragment 2 From Activity")
-
-            replaceFragment(Fragment2(), bundle)
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment, bundle: Bundle)
-    {
-        fragment.arguments = bundle
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
     }
 }
